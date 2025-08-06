7c3beea3bea3b6a40d40c582eabb50a0ac120c2d
little-glitch <fidhakafathima@gmail.com>
2025-08-06 07:16:59 +0530

    cpp program to do comments

---
+++ comments.cpp
@@ -0,0 +1,16 @@
+// this lbrings the iostream library which used for input and output 
+#include <iostream>
+
+/*
+    this block of comment can 
+    caheve multiple line of comments
+    like many lines
+    one thing abput multiline comment you caant nest the comments
+*/
+
+int main() {
+    //it prints the hello world text in the program
+    std::cout << "hello world" << std::endl;
+    return 0;
+}
+//use comments to document your code. dont overdo it though
